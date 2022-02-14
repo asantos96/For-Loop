c9c48b0fe851b2e196a5fccb5e71991ef84c0b29
Andrea Santos <andreasantos@andreas-mbp.myfiosgateway.com>
2022-02-14 16:14:17 -0500

    initial For Loop start

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; ; i++ ){
+            if(i % 2==0 && i % 3==0 && i % 4==0 && i % 5==0);
+            System.out.println ("The answer is " +i);
+            break;
+        }
+    }
+}
